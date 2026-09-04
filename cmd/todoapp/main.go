a04da0486330ee93f5930c2cca26131518a3dab6
Max Khoptyar <intelmax3@gmail.com>
2026-09-04 17:28:13 +0300

    feature/review:  Улучшения

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -8,7 +8,7 @@ import (
 	"syscall"
 
 	core_logger "github.com/skankhunter/todo-go/internal/core/logger"
-	core_postgres_pool "github.com/skankhunter/todo-go/internal/core/repository/postgres/pool"
+	core_pgx_pool "github.com/skankhunter/todo-go/internal/core/repository/postgres/pool/pgx"
 	core_http_middleware "github.com/skankhunter/todo-go/internal/core/transport/http/middleware"
 	core_http_server "github.com/skankhunter/todo-go/internal/core/transport/http/server"
 	users_postgres_repository "github.com/skankhunter/todo-go/internal/features/users/repository/postgres"
@@ -33,7 +33,11 @@ func main() {
 	defer logger.Close()
 
 	logger.Debug("Init postgres connection pool")
-	pool, err := core_postgres_pool.NewConnectionPool(ctx, core_postgres_pool.NewConfigMust())
+
+	pool, err := core_pgx_pool.NewPool(
+		ctx,
+		core_pgx_pool.NewConfigMust(),
+	)
 
 	if err != nil {
 		logger.Fatal("Failed to init postgres connection pool", zap.Error(err))
@@ -54,14 +58,21 @@ func main() {
 		logger,
 		core_http_middleware.RequestID(),
 		core_http_middleware.Logger(logger),
-		core_http_middleware.Panic(),
 		core_http_middleware.Trace(),
+		core_http_middleware.Panic(),
 	)
-	apiVersionRouter := core_http_server.NewAPIVersionRouter(core_http_server.ApiVersion1)
-	apiVersionRouter.RegisterRoutes(usersTransportHTTP.Routes()...)
+	apiVersionRouterV1 := core_http_server.NewAPIVersionRouter(core_http_server.ApiVersion1)
+	apiVersionRouterV1.RegisterRoutes(usersTransportHTTP.Routes()...)
+
+	// apiVersionRouterV2 := core_http_server.NewAPIVersionRouter(
+	// 	core_http_server.ApiVersion2,
+	// 	core_http_middleware.Dummy("API v2 middleware"),
+	// )
+	// apiVersionRouterV2.RegisterRoutes(usersTransportHTTP.Routes()...)
 
 	httpServer.RegisterAPIRouters(
-		apiVersionRouter,
+		apiVersionRouterV1,
+		// apiVersionRouterV2,
 	)
 
 	if err := httpServer.Run(ctx); err != nil {
--- internal/features/users/transport/http/patch_user.go
+++ internal/features/users/transport/http/patch_user.go
@@ -10,7 +10,6 @@ import (
 	core_http_request "github.com/skankhunter/todo-go/internal/core/transport/http/request"
 	core_http_response "github.com/skankhunter/todo-go/internal/core/transport/http/response"
 	core_http_types "github.com/skankhunter/todo-go/internal/core/transport/http/types"
-	core_http_utils "github.com/skankhunter/todo-go/internal/core/transport/http/utils"
 )
 
 type PatchUserRequest struct {
@@ -54,7 +53,7 @@ func (h *UsersHTTPHandler) PatchUser(rw http.ResponseWriter, r *http.Request) {
 	log := core_logger.FromContext(ctx)
 	responseHandler := core_http_response.NewHTTPResponseHandler(log, rw)
 
-	userID, err := core_http_utils.GetIntPathValues(r, "id")
+	userID, err := core_http_request.GetIntPathValues(r, "id")
 	if err != nil {
 		responseHandler.ErrorResponse(
 			err,
@@ -88,8 +87,8 @@ func (h *UsersHTTPHandler) PatchUser(rw http.ResponseWriter, r *http.Request) {
 }
 
 func userPatchFromRequest(request PatchUserRequest) domain.UserPatch {
-	return domain.UserPatch{
-		FullName:    request.FullName.ToDomain(),
-		PhoneNumber: request.PhoneNumber.ToDomain(),
-	}
+	return domain.NewUserPatch(
+		request.FullName.ToDomain(),
+		request.PhoneNumber.ToDomain(),
+	)
 }
